ea4870ad5a07f8e5de36cf146b6404a2ae5efe80
aman9dubey <aman9dubey@gmail.com>
2018-01-07 13:16:59 +0530

    Initial Commit

---
+++ Insurance-classification-Using-Naive-Bayes/nb_plan_name.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+""" 
+    Code for Naive Bayes
+
+    Using a Naive Bayes Classifier to identify insurance plans 
+    
+    authors and labels:
+    Insurance_plan_1 has label 0
+    Insurance_plan_2 has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("/")
+from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+#########################################################
+### your code goes here ###
+
+clf = GaussianNB()
+t0 = time()
+clf.fit(features_train,labels_train)
+print "training time:", round(time()-t0, 3), "s"
+t1 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time()-t1, 3), "s"
+accuracy = accuracy_score(labels_test, pred)
+print(accuracy)
+
+#########################################################
+
+
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Insurance-classification-using-different-classifiers
+
+Naive-Bayes Classifier for training and prediction of suitable insurance plan.
